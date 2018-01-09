0dc0fb835b5673c26ea5363748fd986c01704ab7
missxa <aliona.kharchenko@gmail.com>
2018-01-10 00:25:34 +0100

    transfer function loading properly

--- control.py
+++ control.py
@@ -1,9 +1,10 @@
 from roboy_communication_middleware.msg import MotorCommand, MotorStatus, JointStatus
+from std_msgs.msg import Int32
 
 @nrp.MapRobotPublisher('motor_command',Topic('/roboy/middleware/MotorCommand', MotorCommand))
 @nrp.MapRobotSubscriber('motor_status', Topic('/roboy/middleware/MotorStatus', MotorStatus))
-@nrp.MapRobotSubscriber('joint_status', Topic('/roboy/middleware/MotorStatus', JointStatus))
+# @nrp.MapRobotSubscriber('joint_status', Topic('/roboy/middleware/JointStatus', JointStatus))
 @nrp.Robot2Neuron()
 
-def transferfunction():
-	x=MotorStatus()
+def control(t, motor_status, motor_command):
+	x=1
