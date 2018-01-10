5cd1c69978d884dd775daabb6153c0e08d6eff4e
missxa <aliona.kharchenko@gmail.com>
2018-01-11 00:30:07 +0100

    publishing/subscribing with TF

--- control.py
+++ control.py
@@ -1,5 +1,5 @@
 from roboy_communication_middleware.msg import MotorCommand, MotorStatus, JointStatus
-from std_msgs.msg import Int32
+
 
 @nrp.MapRobotPublisher('motor_command',Topic('/roboy/middleware/MotorCommand', MotorCommand))
 @nrp.MapRobotSubscriber('motor_status', Topic('/roboy/middleware/MotorStatus', MotorStatus))
@@ -7,4 +7,13 @@ from std_msgs.msg import Int32
 @nrp.Robot2Neuron()
 
 def control(t, motor_status, motor_command):
-	x=1
+	from roboy_communication_middleware.msg import MotorCommand, MotorStatus, JointStatus
+	import time
+	time.sleep(0.02)
+	if not isinstance(motor_status.value, type(None)):
+		id = motor_status.value.id
+	command = MotorCommand()
+	command.id = id
+	command.motors = [0,1,2,3]
+	command.setPoints = [1,2,3,4]
+	motor_command.send_message(command)
