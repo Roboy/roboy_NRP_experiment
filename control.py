91a23a462d478bb716e3eac4bc5af0041f40d8fd
missxa <aliona.kharchenko@gmail.com>
2018-01-15 22:17:52 +0100

    hw connection works

--- control.py
+++ control.py
@@ -8,15 +8,16 @@ control_mode = rospy.ServiceProxy("/roboy/middleware/ControlMode", ControlMode, 
 # default control mode: displacement
 control_mode(2,0)
 
+t_prev = 0.0
 
 @nrp.MapRobotPublisher('motor_command',Topic('/roboy/middleware/MotorCommand', MotorCommand))
 @nrp.MapRobotSubscriber('motor_status', Topic('/roboy/middleware/MotorStatus', MotorStatus))
+@nrp.MapVariable("t_prev", global_key="t_prev", initial_value=0.0)
 # @nrp.MapRobotSubscriber('joint_status', Topic('/roboy/middleware/JointStatus', JointStatus))
 # @nrp.MapVariable("control_mode", initial_value=rospy.ServiceProxy("/roboy/middleware/ControlMode", ControlMode, persistent=True))
 @nrp.Robot2Neuron()
 
-def control(t, motor_status, motor_command):
-
+def control(t, t_prev, motor_status, motor_command):
 
 	from roboy_communication_middleware.msg import MotorCommand
 	import time
@@ -26,6 +27,15 @@ def control(t, motor_status, motor_command):
 		id = motor_status.value.id
 	command = MotorCommand()
 	command.id = id
-	command.motors = [0,1,2,3]
-	command.setPoints = [1,2,3,4]
-	motor_command.send_message(command)
+	command.motors = [0]
+	if (t_prev.value==0.0 or t-t_prev.value>10):
+		t_prev.value = t
+		command.setPoints = [2]
+		clientLogger.info("Moving")
+		motor_command.send_message(command)
+	elif (t-t_prev.value>5 and t-t_prev.value<10):
+		command.setPoints = [0]
+		motor_command.send_message(command)
+		clientLogger.info("Not")
+
+
